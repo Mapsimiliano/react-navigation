fe3ebf3e0d570d1966f29d04eac53b0196af422e
Kacper Kafara <kacperkafara@gmail.com>
2024-09-03 10:16:37 +0200

    Fixes for iOS

--- packages/native-stack/src/views/HeaderConfig.tsx
+++ packages/native-stack/src/views/HeaderConfig.tsx
@@ -188,7 +188,12 @@ export function HeaderConfig({
     // Back button menu is not disabled
     headerBackButtonMenuEnabled !== false;
 
-  const isThereCenterView = headerTitleAlign === 'center';
+  const isThereCenterView = Platform.select({
+    ios: headerTitleElement !== null,
+    android: headerTitleAlign === 'center',
+    default: false,
+  })
+  // const isThereCenterView = headerTitleAlign === 'center';
 
   return (
     <ScreenStackHeaderConfig
@@ -245,7 +250,7 @@ export function HeaderConfig({
             </ScreenStackHeaderLeftView>
           ) : null}
           {headerTitleElement != null ? (
-            <ScreenStackHeaderCenterView>
+            <ScreenStackHeaderCenterView style={{ flex: 1 }}>
               {headerTitleElement}
             </ScreenStackHeaderCenterView>
           ) : null}
